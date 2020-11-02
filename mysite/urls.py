55fd509d20fd6912d2f66a8daec887a588b4d31b
integerJI <jjs9536@gmail.com>
2020-11-02 22:24:11 +0900

    test

---
+++ mysite/urls.py
@@ -0,0 +1,8 @@
+# mysite/urls.py
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^chat/', include('chat.urls')),
+]
